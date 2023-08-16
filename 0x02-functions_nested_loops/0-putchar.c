ff8e786ecc24318007993a786cd2ed7588246312
Your Name <you@example.com>
2023-08-15 23:47:22 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
   *main - Entry point
-  *Description: prints _putchar
   *Return: always 0
   */
 int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -14,6 +13,9 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+
 	_putchar('\n');
+
 	return (0);
+
 }
