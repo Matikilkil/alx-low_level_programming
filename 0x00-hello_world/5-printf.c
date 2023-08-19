fbb8838e28e427a76e1deb30b030a826401a510e
Your Name <you@example.com>
2023-08-19 10:17:52 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
   *main - a program that prints line using prinf function
   *Return: 0 (success)
   */
-void main(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
