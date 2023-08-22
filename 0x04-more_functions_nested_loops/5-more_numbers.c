e313b0c651fd0f371cecdd4c1af628aecfeb88ac
Your Name <you@example.com>
2023-08-22 04:01:20 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,21 @@
 #include "main.h"
+
 /**
-  *more_numbers - prints more numbers
-  *Return: void
-  */
+ * more_numbers - print more numbers
+ */
 
 void more_numbers(void)
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
-		{
-			_putchar(j + '0');
-		}
-		_putchar('\n');
+	for (j = 0; j <= 14; j++)
+	{
+	if (j >= 10)
+	_putchar('1');
+	_putchar (j % 10 + '0');
 	}
 	_putchar('\n');
+	}
 }
