ae4bec8fabc7883a04e635bc9b828b02afa9d75f
Your Name <you@example.com>
2024-03-21 13:56:55 -0700

    Delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Deletes the node at index index of a dlistint_t linked list.
- * @head: Pointer to the pointer to the head of the list
- * @index: Index of the node that should be deleted (index starts at 0)
+ * delete_dnodeint_at_index - deletes the node at index of a
+ * dlistint_t linked list
  *
+ * @head: Pointer to the pointer to the head of the list
+ * @index: Index of the new node
  * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
