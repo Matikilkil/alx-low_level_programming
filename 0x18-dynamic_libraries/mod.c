ba494e92206032e6445d50fcbd025b6cac23fa3f
Your Name <you@example.com>
2024-03-25 14:14:37 -0700

    Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * mod - mod of two intergers
+ * @a: first int
+ * @b: second int
+ * Return: final result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * sub - subtract two intergers
+ * @a: first int
+ * @b: second int
+ * Return: subtracted final result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
